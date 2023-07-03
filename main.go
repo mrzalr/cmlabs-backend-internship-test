839b35ecb7614cb8347ce89a50ca13dfd6dab408
mrzalr <mrizalr@hotmail.com>
2023-07-03 15:04:09 +0700

    add fizzbuzz

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	FizzBuzz(100)
+}
+
+func FizzBuzz(loopCount int) {
+	for i := 1; i <= loopCount; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+			continue
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+			continue
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+			continue
+		}
+		fmt.Println(i)
+	}
 }
